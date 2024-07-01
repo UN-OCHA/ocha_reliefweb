79c84b8b36c821db9e3315683a9936474a9f2d89
orakili <github@orakili.net>
2024-07-01 00:41:41 +0000

    feat: add option to hide page title for previews

--- src/Form/ReliefWebResourceTypeForm.php
+++ src/Form/ReliefWebResourceTypeForm.php
@@ -5,10 +5,55 @@ declare(strict_types=1);
 namespace Drupal\ocha_reliefweb\Form;
 
 use Drupal\Core\Entity\BundleEntityFormBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Form handler for ReliefWeb Resource type forms.
  */
 class ReliefWebResourceTypeForm extends BundleEntityFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form = parent::form($form, $form_state);
+
+    $form['settings'] = [
+      '#type' => 'fieldset',
+      '#title' => 'Settings',
+      '#tree' => TRUE,
+    ];
+
+    $form['settings']['disable_page_title'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Disable page title'),
+      '#description' => $this->t('Disable the page title block and use the title from the resource content instead.'),
+      '#default_value' => $this->getEntity()->isPageTitleDisabled(),
+    ];
+
+    return $this->protectBundleIdElement($form);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $entity = $this->getEntity();
+    $entity->set('settings', $form_state->getValue('settings'));
+
+    $status = $entity->save();
+    if ($status == SAVED_UPDATED) {
+      $this->messenger()->addStatus($this->t('The @id resource type has been updated.', [
+        '@id' => $entity->id(),
+      ]));
+    }
+    elseif ($status == SAVED_NEW) {
+      $this->messenger()->addStatus($this->t('The @id resource type has been created.', [
+        '@id' => $entity->id(),
+      ]));
+    }
+
+    $form_state->setRedirectUrl($entity->toUrl('collection'));
+  }
 
 }
